3536e639e3eda89c10a5296f85d0f9b03d1ebf8e
IbrahemAtef <bulbul.20001@gmail.com>
2022-06-26 00:48:39 +0300

    nest project init

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [AuthModule, UserModule],
+})
+export class AppModule {}
