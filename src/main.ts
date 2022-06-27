0361815a76922d0588a34347171a66484251e6fb
IbrahemAtef <bulbul.20001@gmail.com>
2022-06-27 10:43:49 +0300

    validation done

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { ValidateInputPipe } from './core/pipes/validate.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(5000);
 }
 bootstrap();
